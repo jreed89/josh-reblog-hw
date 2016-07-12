7337db5df6516c711f9ac46e2c8fdc050e88e9ee
Joshua Daniel <jrdaniel@ncsu.edu>
2016-07-12 08:36:33 -0400

    2.Create a migration that makes a posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160712123354) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "message"
+    t.string   "mood"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
